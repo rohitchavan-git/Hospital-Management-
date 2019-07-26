9d2bd6dfc3f5fb90c03141c4b98c9229ca36a1e2
rohit <rohit.chavan@dhdigital.co.in>
2019-07-26 16:14:34 +0530

    add Insurance entity

---
+++ src/main/java/com/rohit/chavan/hospital/hospital/entity/Insurance.java
@@ -0,0 +1,38 @@
+package com.rohit.chavan.hospital.hospital.entity;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Insurance pojo for component inheritance
+ * @author Rohit Chavan
+ *
+ */
+@Embeddable
+public class Insurance {
+	private String providerName;
+	private double copay;
+
+	public Insurance() {
+	}
+
+	public Insurance(String providerName, double copay) {
+		this.providerName = providerName;
+		this.copay = copay;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+
+	public double getCopay() {
+		return copay;
+	}
+
+	public void setCopay(double copay) {
+		this.copay = copay;
+	}
+}
